ae784f304bfc623db72f6309fde4ca99b808cff6
Alexandre Marcos <alexandremarcos@gmail.com>
2021-04-04 23:40:25 +0200

    testé

--- java/Main.java
+++ java/Main.java
@@ -10,6 +10,7 @@ public class Main {
 
             g.generateNextState();
             System.out.println(g.toString());
+         
         }
     }
 }
--- java/gameoflife/Grid.java
+++ java/gameoflife/Grid.java
@@ -15,6 +15,7 @@ public class Grid {
 
 
     public Grid(int sizeGrid) {
+        this.cells = new Cell[sizeGrid][sizeGrid];
         this.rd = new Random();
         this.sizeGrid = sizeGrid;
         generateRandomInitialState();
@@ -26,8 +27,15 @@ public class Grid {
     }
 
     private void generateRandomInitialState() {
-
-
+        for (int i = 0; i < sizeGrid; i++) {
+            for (int j = 0; j < sizeGrid; j++) {
+                if (this.rd.nextInt() % 6 == 0) {
+                    this.cells[i][j] = new Cell(false);
+                } else {
+                    this.cells[i][j] = new Cell(true);
+                }
+            }
+        }
     }
 
     private void Corner(int i, int j) { // Check all corners
@@ -46,6 +54,27 @@ public class Grid {
             if (cells[sizeGrid - 1][sizeGrid - 2].isAlive()) NbAlive++;
             if (cells[sizeGrid - 2][sizeGrid - 1].isAlive()) NbAlive++;
             if (cells[sizeGrid - 2][sizeGrid - 2].isAlive()) NbAlive++;
+
+            isCellAlive = Cell.processState(cells[i][j].isAlive(), NbAlive);
+            tmpC[i][j].setIsAlive(isCellAlive);
+
+        }
+
+
+        if (i == sizeGrid - 1 && j == 0) {       // For the bottom-left corner
+            if (cells[sizeGrid - 2][0].isAlive()) NbAlive++;
+            if (cells[sizeGrid - 2][1].isAlive()) NbAlive++;
+            if (cells[sizeGrid - 1][1].isAlive()) NbAlive++;
+
+            isCellAlive = Cell.processState(cells[i][j].isAlive(), NbAlive);
+            tmpC[i][j].setIsAlive(isCellAlive);
+
+        }
+
+        if (i == 0 && j == sizeGrid - 1) {       // For the top-right corner
+            if (cells[0][sizeGrid - 2].isAlive()) NbAlive++;
+            if (cells[1][sizeGrid - 2].isAlive()) NbAlive++;
+            if (cells[1][sizeGrid - 1].isAlive()) NbAlive++;
 
             isCellAlive = Cell.processState(cells[i][j].isAlive(), NbAlive);
             tmpC[i][j].setIsAlive(isCellAlive);
@@ -75,7 +104,6 @@ public class Grid {
 
             isCellAlive = Cell.processState(cells[i][j].isAlive(), NbAlive);
             tmpC[i][j].setIsAlive(isCellAlive);
-            System.out.println(" 94" + NbAlive);
         }
         // System.out.println(cells[i][j]);
 
@@ -103,7 +131,7 @@ public class Grid {
         }
     }
 
-    private void avoidDupli(int i, int j) {
+    private void avoidDupli(int i, int j) {     // Use to don't duplicate the code
         if (cells[i - 1][j].isAlive()) NbAlive++;
         if (cells[i - 1][j + 1].isAlive()) NbAlive++;
         if (cells[i][j + 1].isAlive()) NbAlive++;
@@ -111,7 +139,7 @@ public class Grid {
         if (cells[i + 1][j].isAlive()) NbAlive++;
     }
 
-    private void cellIn(int i, int j) {
+    private void cellIn(int i, int j) {     // For Cell in the Array (not corner or side)
 
         if (i != 0 && j != 0 && j != sizeGrid - 1 && i != sizeGrid - 1) {
 
@@ -127,21 +155,19 @@ public class Grid {
     }
 
     public void generateNextState() {
-        tmpC = cells.clone();
+        this.tmpC = new Cell[this.sizeGrid][this.sizeGrid];
+        for (int a = 0; a < this.sizeGrid; a++) {
+            for (int b = 0; b < this.sizeGrid; b++) {
+                this.tmpC[a][b] = new Cell(cells[a][b].isAlive());
+            }
+        }
         for (int i = 0; i < sizeGrid; i++) {
             for (int j = 0; j < sizeGrid; j++) { // Select the cell to look on
 
                 side(i, j);
                 Corner(i, j);
                 cellIn(i, j);
-
                 NbAlive = 0;
-            }
-        }
-        for (int i = 0; i < sizeGrid; i++) {
-            for (int j = 0; j < sizeGrid; j++) { // Select the cell to look on
-                System.out.println("tmp : " + tmpC[i][j]);
-                System.out.println("cells :" + cells[i][j]);
             }
         }
         cells = tmpC.clone();
@@ -153,14 +179,14 @@ public class Grid {
             Cell[] myRow = this.cells[i]; // get 1 row of cells
             for (int j = 0; j < myRow.length; j++) {
                 if (myRow[j] == myRow[myRow.length - 1]) {    // if the last cell of the row
-                    this.temp = this.temp + myRow[j];            // We just add add the cell to the string
+                    this.temp = this.temp + myRow[j];            // We just add the cell to the string
                 } else this.temp = this.temp + myRow[j] + " ";  // Else we put the cell to the string + a space
             }
             if (i != sizeGrid - 1) {
                 this.temp = this.temp + "\n"; // If its not the last row we put an \n
             }
         }
-        System.out.println(this.temp);
+
         return this.temp;
     }
 }
